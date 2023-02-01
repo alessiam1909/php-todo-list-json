25b87a9155dae5fa269f4264f57016e2f054f768
alessiam1909 <ale.menon.vr@gmail.com>
2023-02-01 17:12:10 +0100

    stampa array in pagina

--- index.php
+++ index.php
@@ -5,12 +5,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.3.0/axios.min.js" integrity="sha512-A6BG70odTHAJYENyMrDN6Rq+Zezdk+dFiFFN6jH1sB+uJT3SYMV4zDSVR+7VawJzvq7/IrT/2K3YWVKRqOyN0Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <link rel="stylesheet" href="./css/style.css">
     <title>To-Do list php</title>
 </head>
 <body>
-    <div id="app"></div>
+    <div id="app">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1>Todo-List</h1>
+                    <ul>
+                        <li v-for="todo in todoList" >
+                            {{todo.language}}
+                        </li>
+                    </ul>
+                    <form>
+                        <input type="text" v-model="language" placeholder="Aggiungi elemento">
+                        <button type="submit" @click="addToDoItem">Inserisci elemento</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.3.0/axios.min.js" integrity="sha512-A6BG70odTHAJYENyMrDN6Rq+Zezdk+dFiFFN6jH1sB+uJT3SYMV4zDSVR+7VawJzvq7/IrT/2K3YWVKRqOyN0Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="./js/script.js"></script>
+
 </body>
 </html>
